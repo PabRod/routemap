773449184b75f5f40e7d325af97ecafe8d5dc769
Pablo Rodríguez-Sánchez <pablo.rodriguez.sanchez@gmail.com>
2018-08-18 17:02:58 +0200

    Removed city points

--- script.R
+++ script.R
@@ -23,10 +23,8 @@ colnames(coords) <- c('lon', 'lat')
 
 ## Get the cities
 nl.cities <- filter(world.cities, country.etc == 'Netherlands')
-nl.cities.big <- filter(nl.cities, pop > 200000)
 nl.selection <- filter(nl.cities, name %in% c('Amsterdam', 'Rotterdam', 'Utrecht', 'Alkmaar', 'Middelburg', 'Dordrecht', 'Wageningen'))
 be.cities <- filter(world.cities, country.etc == 'Belgium')
-be.cities.big <- filter(be.cities, pop > 150000)
 be.selection <- filter(be.cities, name %in% c('Gent', 'Ostend', 'Brugge'))
 
 ## Plot the map
@@ -35,8 +33,6 @@ p <- ggmap(map)
 p <- p + geom_point(data = coords, aes(x = lon, y = lat), alpha = 0.5, color = 'red')
 p <- p + theme(legend.position = 'right') 
 p <- p + labs(x = 'Longitude', y = 'Latitude', title = 'Location History') 
-p <- p + geom_point(data = nl.selection, aes(x = long, y =lat), alpha = 0.7, show.legend = FALSE)
-p <- p + geom_point(data = be.selection, aes(x = long, y =lat), alpha = 0.7, show.legend = FALSE)
 p <- p + geom_text(data = nl.selection, aes(x=long, y=lat, label=name, family='serif', size=5), check_overlap = TRUE, angle = 30, show.legend = FALSE)
 p <- p + geom_text(data = be.selection, aes(x=long, y=lat, label=name, family='serif', size=5), check_overlap = TRUE, angle = 30, show.legend = FALSE)
 
